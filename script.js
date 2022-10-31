4b92a5777715c3382b76bab60da9e9aea5210309
Luke Liverpool <lliverpool86@gmail.com>
2022-10-31 17:30:15 -0400

    added a round function

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 function getComputerChoice() {
-    const choices = ['Rock', 'Paper', 'Scissors'];
+    const choices = ['rock', 'paper', 'scissors'];
     const random = Math.floor(Math.random() * choices.length);
     return choices[random];
 }
-console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    
+    if (playerSelection === computerSelection) {
+        return "It's a tie";
+      } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return 'You lose! Paper beats Rock';
+      } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You win! Rock beats Scissors';
+      } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'You lose! Scissors beats Paper';
+      } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You win! Paper beats Rock';
+      } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'You lose! Rock beats Scissors';
+      } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'You win! Scissors beats Paper';
+      }
+    
+}
+
+const playerSelection = 'paper';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
